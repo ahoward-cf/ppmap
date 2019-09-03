f9b21627501c1d4613c227e5e9109a34180445cc
ahoward-cf <alexander.howard@astro.cf.ac.uk>
2019-09-03 14:28:11 +0100

    Add EQUINOX key
    Added an equinox key to output header files so DS9 doesnt assume its 1950 (why does it assume its 1950?!)

--- source/writeimage2d.f
+++ source/writeimage2d.f
@@ -87,6 +87,8 @@ C  Write other optional keywords to the header
      &  'coordinate increment along axis',status)
       call ftpkyf(unit,'CROTA2',crota2,6,
      &  'coordinate system rotation angle',status)
+           call ftpkyf(unit,'EQUINOX',2000,6,
+     &  'Default equinox of system - 2000',status)
 
 C  The FITS file must always be closed before exiting the program. 
 C  Any unit numbers allocated with FTGIOU must be freed with FTFIOU.
